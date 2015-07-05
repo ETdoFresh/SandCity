c611be955907ca8e02a35c11756a615281fc802b
ETdoFresh <ETdoFresh@gmail.com>
2015-07-05 11:13:18 -0500

    Created a simple movement networked arena with a cube for players to push around. Physics relies on the server, so it may feel a bit unresponsive on the client until optimizations can be placed in.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    public float movementAxisScale = 20f;
+    public float rotationAxisScale = 4f;
+    public float jumpAxisScale = 20f;
+
+    Rigidbody _rigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+
+        if (Component.FindObjectOfType<NetworkManager>() != null)
+            Debug.Log("Found Network Manager");
+        else
+            Debug.Log("Did not find Network Manager");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        SendInput();
+    }
+
+    [Client]
+    void SendInput()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0 || Mathf.Abs(Input.GetAxis("Vertical")) > 0 || Input.GetAxis("Jump") > 0)
+            CmdAdjustForce(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), Input.GetAxis("Jump"));
+    }
+
+    [Command]
+    void CmdAdjustForce(float horizontal, float vertical, float jump)
+    {
+        _rigidBody.AddRelativeForce(Vector3.forward * vertical * movementAxisScale);
+        _rigidBody.AddRelativeTorque(Vector3.up * horizontal * rotationAxisScale);
+        _rigidBody.AddRelativeForce(Vector3.up * jump * jumpAxisScale);
+    }
+}
