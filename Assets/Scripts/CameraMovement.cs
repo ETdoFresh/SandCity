2a529b68377fc51d07879f8d34f54ac61bbc7766
ETdoFresh <ETdoFresh@gmail.com>
2015-07-17 00:34:24 -0500

    Making some progress on the digging, but still not exactly sure if I like the digging. Using height map, and it's basically working. But slow and big! Will look into it later, but for now, marching onwards!

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Vector3 lowerLimits = new Vector3(-250, 5, -250);
+    public Vector3 upperLimits = new Vector3(250, 40, 250);
+    
+    Vector3 _fire2Down;
+    Vector3 _startingPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        _startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = transform.position;
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            position.x += Input.GetAxis("Horizontal");
+            position.z += Input.GetAxis("Vertical");
+        }
+        else if (Input.GetButton("Fire2"))
+        {
+            if (Input.GetButtonDown("Fire2"))
+                _fire2Down = Input.mousePosition;
+            Vector3 deltaPosition = Input.mousePosition - _fire2Down;
+            deltaPosition *= Time.deltaTime;
+            position.x += deltaPosition.x;
+            position.z += deltaPosition.y;
+        }
+        position.y += -Input.GetAxis("Mouse ScrollWheel") * 20;
+        position.x = Mathf.Clamp(position.x, lowerLimits.x, upperLimits.x);
+        position.y = Mathf.Clamp(position.y, lowerLimits.y, upperLimits.y);
+        position.z = Mathf.Clamp(position.z, lowerLimits.z, upperLimits.z);
+        transform.position = position;
+    }
+
+    public void ResetPosition()
+    {
+        transform.position = _startingPosition;
+    }
+}
--- Assets/Scripts/TerrainDig.cs
+++ Assets/Scripts/TerrainDig.cs
@@ -1,57 +1,127 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TerrainDig : MonoBehaviour {
+public class TerrainDig : MonoBehaviour
+{
+    public int brushSize = 10;
+    public float brushOpacity = 0.0005f;
+    public float minDistanceUntilNextUpdate = 0.5f;
+
     Terrain _terrain;
-    int _xRes;
-    int _yRes;
+    Vector3 _lastUpdatePosition;
 
     void Start()
     {
-        _terrain = GameObject.Find("MainTerrain").GetComponent<Terrain>();
+        _terrain = GetComponent<Terrain>();
+        Debug.Log(arrayToString(RectangleArray2D(10, 10)));
+        Debug.Log(arrayToString(EllipseArray2D(10, 10)));
+        Debug.Log(arrayToString(Gaussian2D(10, 10)));
+    }
 
-        //get terain heightmap width and height
-        _xRes = _terrain.terrainData.heightmapWidth;
-        _yRes = _terrain.terrainData.heightmapHeight;
+    public void ResetTerrain()
+    {
+        int width = _terrain.terrainData.heightmapWidth;
+        int height = _terrain.terrainData.heightmapHeight;
+        float[,] heightMap = _terrain.terrainData.GetHeights(0, 0, width, height);
+
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++)
+                heightMap[i, j] = 0.5f;
+
+        _terrain.terrainData.SetHeights(0, 0, heightMap);
     }
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1"))
         {
+            if (Input.GetButtonDown("Fire1"))
+                _lastUpdatePosition = new Vector3(float.NegativeInfinity, float.NegativeInfinity, float.NegativeInfinity);
 
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
             {
-                int x = Mathf.FloorToInt((hit.point.x + 40) / 80 * _xRes);
-                int y = Mathf.FloorToInt((hit.point.z + 40) / 80 * _yRes);
-                DigTenPixelSquare(x, y);
+                if (Vector3.Distance(hit.point, _lastUpdatePosition) >= minDistanceUntilNextUpdate)
+                {
+                    //TODO: Cleanup Edge cases
+
+                    _lastUpdatePosition = hit.point;
+
+                    float x = (hit.point.x - transform.position.x) / _terrain.terrainData.size.x;
+                    float y = (hit.point.z - transform.position.z) / _terrain.terrainData.size.z;
+
+                    // Digging...
+                    int leftBrush = (int)(x * _terrain.terrainData.heightmapWidth - brushSize / 2);
+                    int topBrush = (int)(y * _terrain.terrainData.heightmapHeight - brushSize / 2);
+
+                    float[,] heightMap = _terrain.terrainData.GetHeights(leftBrush, topBrush, brushSize, brushSize);
+                    float[,] brush = EllipseArray2D(brushSize, brushSize);
+
+                    for (int i = 0; i < brushSize; i++)
+                        for (int j = 0; j < brushSize; j++)
+                            heightMap[i, j] -= brush[i, j] * brushOpacity;
+
+                    _terrain.terrainData.SetHeights(leftBrush, topBrush, heightMap);
+                }
             }
         }
     }
 
-    void DigTenPixelSquare(int x, int y)
+    float[,] RectangleArray2D(int width, int height)
     {
-        int xLeft = Mathf.Max(0, x - 5);
-        int yTop = Mathf.Max(0, y - 5);
-        int xWidth = Mathf.Min(_xRes - xLeft, 10);
-        int yHeight = Mathf.Min(_yRes - yTop, 10);
+        float[,] rectangleArray = new float[width, height];
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
+                rectangleArray[x, y] = 1;
+        return rectangleArray;
+    }
 
-        //GetHeights - gets the heightmap points of the terrain.
-        float[,] heightMap = _terrain.terrainData.GetHeights(xLeft, yTop, xWidth, yHeight);
-        float digDepth = .02f;
+    float[,] EllipseArray2D(int width, int height)
+    {
+        float[,] circleArray = new float[width, height];
+        float a = width / 2;
+        float b = height / 2;
+        float x0 = width / 2;
+        float y0 = height / 2;
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
+                if (Mathf.Pow(x - x0, 2) / Mathf.Pow(a, 2) + Mathf.Pow(y - y0, 2) / Mathf.Pow(b, 2) <= 1)
+                    circleArray[x, y] = 1;
+        return circleArray;
+    }
 
-        string debugStr = "";
-        for (int i = 0; i < xWidth; i++)
-            for (int j = 0; j < yHeight; j++)
+    float[,] Gaussian2D(int width, int height, float hardness = 0)
+    {
+        float[,] gaussian2DArray = new float[width, height];
+
+        float x0 = width / 2;
+        float y0 = height / 2;
+        float stdx = width;
+        float stdy = height;
+
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
             {
-                debugStr += "(" + i + "," + j + ") " + heightMap[i, j] + "|";
-                heightMap[i, j] = Mathf.Max(0, heightMap[i, j] - digDepth);
+                gaussian2DArray[x, y] = 1 * Mathf.Exp(-(Mathf.Pow(x - x0, 2) / (2 * Mathf.Pow(stdx, 2)) + Mathf.Pow(y - y0, 2) / (2 * Mathf.Pow(stdy, 2))));
             }
 
-                Debug.Log(debugStr);
+        return gaussian2DArray;
+    }
 
-        _terrain.terrainData.SetHeights(xLeft, yTop, heightMap);
+    string arrayToString(float[,] array2D)
+    {
+        string str = "[";
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            for (int i = 0; i < array2D.GetLength(0); i++)
+            {
+                str += "{" + array2D[i, j] + "}";
+                if (i != array2D.GetLength(0) - 1) str += ", ";
+            }
+            if (j != array2D.GetLength(1) - 1) str += " \n ";
+        }
+        str += "]";
+        return str;
     }
 }
